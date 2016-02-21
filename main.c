74eb75ee69447d1cd50c33ad795076095c691317
Basil Bogonatov <ween1991@gmail.com>
2016-02-21 15:32:37 +0000

    echo

--- main.c
+++ main.c
@@ -7,44 +7,48 @@
 #include <ev.h>
 
 struct server_opts {
-    char* host;
-    int port;
-    char* dir;
+	char* host;
+	int port;
+	char* dir;
 } opts;
 
 void init_opts(int argc, char** argv) {
-    opts.host = 0;
-    opts.port = 0;
-    opts.dir = 0;
+	opts.host = 0;
+    	opts.port = 0;
+    	opts.dir = 0;
 
-    int c;
-    opterr = 0;
-    while ((c = getopt (argc, argv, "h:p:d:")) != -1)
-        switch (c) {
-            case 'h':
-                opts.host = optarg;
-            break;
-            case 'p':
-                opts.port = atoi(optarg);
-            break;
-            case 'd':
-                opts.dir = optarg;
-            break;
-        }
+    	int c;
+   	opterr = 0;
+    	while ((c = getopt (argc, argv, "h:p:d:")) != -1)
+        	switch (c) {
+            		case 'h':
+                		opts.host = optarg;
+            		break;
+            		case 'p':
+                		opts.port = atoi(optarg);
+            		break;
+            		case 'd':
+                		opts.dir = optarg;
+            		break;
+        	}
 
-    printf("Current server options: host = %s, port = %d, dir = %s\n", opts.host, opts.port, opts.dir);
+    	printf("Current server options: host = %s, port = %d, dir = %s\n", opts.host, opts.port, opts.dir);
 
-    if (!(opts.host && opts.port && opts.dir)) {
-        printf("Please specify all server options.\n");
-        abort();
-    }
+    	if (!(opts.host && opts.port && opts.dir)) {
+        	printf("Please specify all server options.\n");
+        	abort();
+    	}
 }
 
 
 void read_cb(struct ev_loop* loop, struct ev_io* watcher, int revents) {
-	char buf[] = "zomg lol test\n";
-	send(watcher->fd, buf, sizeof(buf), MSG_NOSIGNAL);
-
+	char buf[1024];
+	
+	int r = recv(watcher->fd, buf, sizeof(buf), MSG_NOSIGNAL);
+	if (r > 0) {
+		send(watcher->fd, buf, r, MSG_NOSIGNAL);
+	}
+	
 	ev_io_stop(loop, watcher);
 	shutdown(watcher->fd, SHUT_RDWR);
 	free(watcher);
@@ -81,27 +85,27 @@ void loop() {
 }
 
 void daemonize() {
-    pid_t pid, sid;
-    if (!fork()) {
-        umask(0);
-        sid = setsid();
-        close(STDIN_FILENO);
-        close(STDOUT_FILENO);
-        close(STDERR_FILENO);
+    	pid_t pid, sid;
+    	if (!fork()) {
+        	umask(0);
+       		sid = setsid();
+        	close(STDIN_FILENO);
+        	close(STDOUT_FILENO);
+        	close(STDERR_FILENO);
 
-        loop();
-    } else {
-        exit(0);
-    }
+        	loop();
+    	} else {
+        	exit(0);
+    	}
 }
 
 int main(int argc, char** argv) {
-    init_opts(argc, argv);
+    	init_opts(argc, argv);
 
-    printf("Starting server!\n");
+    	printf("Starting server!\n");
 
-    //daemonize();
-    loop();
+    	//daemonize();
+    	loop();
 
-    return 0;
+   	return 0;
 }
